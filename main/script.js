4e07d3e24a7b895b989b5faa43881808e830f8fe
Arnau <4bvives@gmail.com>
2023-03-08 11:32:06 +0100

    particles

---
+++ main/script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+    Particles.init({
+      selector: '.background',
+      //color: 'red',
+      connectParticles: true,
+      responsive:true
+    });
+  };
+  //https://github.com/marcbruederlin/particles.js
